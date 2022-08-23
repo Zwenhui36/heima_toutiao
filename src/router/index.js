dc3e1d5e32f27156ab55721de096b0b895b38f13
wenhuizheng <wenhuizheng36@qq.com>
2022-08-23 16:09:37 +0800

    1.初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// 导入需要的组件
+import Home from '@/views/Home/Home.vue'
+import User from '@/views/User/User.vue'
 
+// 把 VueRouter 安装为 Vue 的插件
 Vue.use(VueRouter)
 
+// 路由规则的数组
 const routes = [
+  // 重定向
+  { path: '/', redirect: '/home' },
+  // 定义首页的路由规则
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/home',
+    component: Home
   },
+  // 定义我的的路由规则
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/user',
+    component: User
   }
 ]
 
+// 创建路由实例对象
 const router = new VueRouter({
-  routes
+  routes: routes // 也可以简写为routes
 })
 
 export default router
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home-container">
+    <van-nav-bar title="杰哥资讯" fixed />
+    <h3>4</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>3</h3>
+    <h3>4</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="less" scoped>
+.home-container {
+  padding: 46px 0 50px 0;
+}
+.van-nav-bar {
+  background-color: #82c3ff;
+}
+// 第三方组件 原有样式覆盖不了 就在前面加/deep/
+/deep/ .van-nav-bar__title {
+  color: rgb(255, 255, 255);
+}
+</style>
